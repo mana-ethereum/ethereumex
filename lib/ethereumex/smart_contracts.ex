b246134849b8b8436b709367517d9fe2c7a743a0
Ayrat Badykov <ayratin555@gmail.com>
2017-10-02 23:35:24 +0300

    add smart contract deploy

--- lib/ethereumex/smart_contracts.ex
+++ lib/ethereumex/smart_contracts.ex
@@ -8,6 +8,38 @@ defmodule Ethereumex.SmartContracts do
     |> compile_request(language)
   end
 
+  @spec deploy(binary, keyword) :: binary
+  def deploy(compiled_contract, opts \\ []) do
+    from = if is_nil(opts[:from]), do: coinbase(), else: opts[:from]
+    gas = if is_nil(opts[:gas]) do
+        estimated_gas(compiled_contract, from)
+      else
+        opts[:gas]
+      end
+
+    {:ok, %{result: result}} =
+      [%{from: from, gas: gas, data: compiled_contract}]
+      |> HttpClient.eth_send_transaction()
+
+    result
+  end
+
+  @spec coinbase() :: binary
+  defp coinbase do
+    {:ok, %{"result" => result}} = HttpClient.eth_coinbase
+
+    result
+  end
+
+  @spec estimated_gas(binary, binary) :: binary
+  defp estimated_gas(compiled_contract, from) do
+    {:ok, %{"result" => result}} =
+      [%{from: from, data: compiled_contract}]
+      |> HttpClient.eth_estimate_gas
+
+    result
+  end
+
   @spec read_file(path :: binary) :: binary
   defp read_file(path) do
     {:ok, contract_string} = path |> File.read
