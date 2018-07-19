5aae33d7eeefe5649d28585276e245415a915a3f
Zdenko Nevrala <nevralaz@gmail.com>
2018-07-19 23:38:22 +0200

    Next tests.

--- test/ethereumex/client/macro_test.exs
+++ test/ethereumex/client/macro_test.exs
@@ -51,6 +51,16 @@ defmodule Ethereumex.Client.MacroTest do
   end
 
   @address "0x407d73d8a49eeb85d32cf465507dd71d507100c1"
+  @hash "0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238"
+  @hex_232 "0xe8"
+  @transaction %{
+    from: @address,
+    to: @address,
+    gas: @hex_232,
+    gasPrice: @hex_232,
+    value: @hex_232,
+    data: @hash
+  }
 
   test ".web3_client_version/0", do: Helpers.check("web3_client_version")
   test ".net_version/0", do: Helpers.check("net_version")
@@ -75,6 +85,20 @@ defmodule Ethereumex.Client.MacroTest do
   test ".shh_new_identity/0", do: Helpers.check("shh_new_identity")
   test ".shh_new_group/0", do: Helpers.check("shh_new_group")
 
+  describe "eth_get_block_transaction_count_by_number/0" do
+    test "w/o params",
+      do: Helpers.check("eth_get_block_transaction_count_by_number", [], ["latest"])
+
+    test "with number", do: Helpers.check("eth_get_block_transaction_count_by_number", [@hex_232])
+  end
+
+  describe "eth_get_uncle_count_by_block_number/0" do
+    test "w/o params",
+      do: Helpers.check("eth_get_uncle_count_by_block_number", [], ["latest"])
+
+    test "with number", do: Helpers.check("eth_get_uncle_count_by_block_number", [@hex_232])
+  end
+
   test ".web3_sha3/1", do: Helpers.check("web3_sha3", ["string to be hashed"])
 
   test ".eth_get_balance/1",
@@ -86,9 +110,45 @@ defmodule Ethereumex.Client.MacroTest do
   test ".eth_get_transaction_count/1",
     do: Helpers.check("eth_get_transaction_count", [@address], ["latest"])
 
-  test ".eth_get_block_transaction_count_by_hash/1" do
-    Helpers.check("eth_get_block_transaction_count_by_hash", [
-      "0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238"
-    ])
-  end
+  test ".eth_get_block_transaction_count_by_hash/1",
+    do: Helpers.check("eth_get_block_transaction_count_by_hash", [@hash])
+
+  test ".eth_get_uncle_count_by_block_hash/1",
+    do: Helpers.check("eth_get_uncle_count_by_block_hash", [@hash])
+
+  test ".eth_get_code/1",
+    do: Helpers.check("eth_get_code", [@address], ["latest"])
+
+  test ".eth_sign/2",
+    do: Helpers.check("eth_sign", [@address, "data to sign"])
+
+  test ".eth_send_transaction/1",
+    do: Helpers.check("eth_send_transaction", [@transaction])
+
+  test ".eth_send_raw_transaction/1",
+    do: Helpers.check("eth_send_raw_transaction", [@hash])
+
+  test ".eth_call/1",
+    do: Helpers.check("eth_call", [@transaction], ["latest"])
+
+  test ".eth_estimate_gas/1",
+    do: Helpers.check("eth_estimate_gas", [@transaction], ["latest"])
+
+  test ".eth_get_block_by_hash/2",
+    do: Helpers.check("eth_get_block_by_hash", [@hash, false])
+
+  test ".eth_get_block_by_number/2",
+    do: Helpers.check("eth_get_block_by_number", [@hex_232, false])
+
+  test ".eth_get_transaction_by_hash/1",
+    do: Helpers.check("eth_get_transaction_by_hash", [@hash])
+
+  test ".eth_get_transaction_by_block_hash_and_index/2",
+    do: Helpers.check("eth_get_transaction_by_block_hash_and_index", [@hash, "0x0"])
+
+  test ".eth_get_transaction_by_block_number_and_index/2",
+    do: Helpers.check("eth_get_transaction_by_block_number_and_index", ["0x29c", "0x0"])
+
+  test ".eth_get_transaction_receipt/1",
+    do: Helpers.check("eth_get_transaction_receipt", [@hash])
 end
