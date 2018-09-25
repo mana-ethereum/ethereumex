7b0a278f549a149cac34e46e5a8222925b7c932f
Ayrat Badykov <ayratin555@gmail.com>
2018-09-25 16:36:51 +0300

    bump elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ethereumex.Mixfile do
     [
       app: :ethereumex,
       version: "0.3.4",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       description: "Elixir JSON-RPC client for the Ethereum blockchain",
       package: [
         maintainers: ["Ayrat Badykov", "Izel Nakri", "Geoff Hayes"],
@@ -31,7 +31,7 @@ defmodule Ethereumex.Mixfile do
       {:httpoison, "~> 1.1.0"},
       {:poison, "~> 3.1.0"},
       {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
