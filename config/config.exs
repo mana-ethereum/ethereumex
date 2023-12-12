2b768978a3da494c4fa44175c026f73862b9f97c
Ayrat Badykov <ayratin555@gmail.com>
2023-12-12 20:43:18 +0200

    chore: add json header by default

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@ import Config
 config :ethereumex,
   http_options: [pool_timeout: 5000, receive_timeout: 15_000],
   http_pool_options: %{},
-  http_headers: [{"Content-Type", "application/json"}]
+  http_headers: []
 
 # config :ethereumex, ipc_path: "/Library/Application Support/io.parity.ethereum/jsonrpc.ipc"
 import_config "#{Mix.env()}.exs"
